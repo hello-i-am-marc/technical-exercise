6700ccc6920cf22438ea52c8a6634f6f9e287f27
hello-i-am-marc <291031668+hello-i-am-marc@users.noreply.github.com>
2026-06-05 13:29:03 -0400

    updating for deprecated lock parameters

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -36,7 +36,7 @@ resource "aws_s3_bucket_public_access_block" "tf_state" {
   restrict_public_buckets = true
 }
 
-resource "aws_dynamodb_table" "tf_lock" {
+/* resource "aws_dynamodb_table" "tf_lock" {
   name         = "${var.project}-tflock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
@@ -44,7 +44,7 @@ resource "aws_dynamodb_table" "tf_lock" {
     name = "LockID"
     type = "S"
   }
-}
+} */
 
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
