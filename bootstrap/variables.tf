81d32c3f579df0a96cba9762bcc4c56b69199c89
hello-i-am-marc <291031668+hello-i-am-marc@users.noreply.github.com>
2026-06-05 11:00:38 -0400

    bootstrap v1

---
+++ bootstrap/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project" {
+  type    = string
+  default = "tech-exercise"
+}
+
+variable "github_owner" {
+  type = string
+}
+
+variable "github_repo" {
+  type    = string
+  default = "technical-exercise"
+}
